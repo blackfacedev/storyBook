ad53dcc3e0b97498d0d141356b7e1a97f348501d
Marcel Assis <marcel@marcelassis.com>
2019-07-11 22:50:05 -0300

    Adding Google oAuth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+// Passport config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
+
+// Use routes
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
   res.send('It works');
 });
+
+// ======== Everything above =========
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://marcelassis:cuba2000@cluster0-bvg53.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '418238309691-vejkkuo0lk9hjmn1eht1fo6tcla63u9a.apps.googleusercontent.com',
+  googleClientSecret: '7vYExFEbriYQokUtlF9y81KR'
+};
